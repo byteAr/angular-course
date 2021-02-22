ac3a90a5045a1a7e4b595d4dbb5dcdee21cd9132
Marcos Lopez <marcosmatiaslopez.2018@gmail.com>
2021-02-22 20:48:13 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './conatdor/contador/contador.module';
+
 @NgModule({
+  //aca van los componentes que creamos en nuestra aplicacion
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heroe/heroe.componnet.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+
+export class HeroeComponent {
+    
+    nombre: string  = 'Iroman';
+    edad  : number = 45;
+
+    get nombreCpaitalizado(): string {
+        return this.nombre.toLocaleUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${ this.nombre } - ${ this.edad }`
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30
+    }
+
+
+}
